c3d93e72c11d30cd6263bf9bccfe44bb6fa46f79
Tom Austin <Tom.Austin@streets-heaver.com>
2020-09-04 16:41:03 +0100

    Progress on Publish-VMs

--- Private/Publish-VMs.ps1
+++ Private/Publish-VMs.ps1
@@ -16,36 +16,37 @@ function Publish-VMs {
         }
         else {
             #Win RM Deploy
+
             
             if ($HyperVServers.Count -eq 1) {
-                $hyperVServer = $HyperVServers[0].Name
+                $hyperVIndex = 0
             }
             else {
-                
+
                 if (!$hyperVIndex) {
                     #First iteration so just use first Hyper-V Server
-                    $hyperVServer = $HyperVServers[0].Name
                     $hyperVIndex = 0
+                    
                 }
                 else {
                     $reqIndex = $hyperVIndex + 1
                     if ($HyperVServers[$reqIndex]) {
-                        $hyperVServer = $HyperVServers[$reqIndex].Name
                         $hyperVIndex = $reqIndex
                     }
                     else {
-                        $hyperVServer = $HyperVServers[0].Name
                         $hyperVIndex = 0
                     }
                 }
 
-                #Check Server has capacity
-                if ($HyperVServers[$hyperVIndex].MaxVMCount -gt 0){
+            }
+
+            #Check Server has capacity
+            if ($HyperVServers[$hyperVIndex].MaxVMCount -gt 0) {
+                if ((Get-VM -ComputerName $HyperVServers[$hyperVIndex].Name | Measure-Object).Count -ge $HyperVServers[$hyperVIndex].MaxVMCount) {
                     
                 }
-                
-
             }
+
             Submit-VM -VM $vm -HyperVServer $hyperVServer
         }
     }
