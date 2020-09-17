cfba57607a6e7c5ae89a1486a0a55e5fbc57a1f3
Tom Austin <Tom.Austin@streets-heaver.com>
2020-09-17 08:14:53 +0100

    Added all Test-DefinitionFile tests

--- Tests/Test-DefinitionFile.Tests.ps1
+++ Tests/Test-DefinitionFile.Tests.ps1
@@ -42,5 +42,146 @@ Describe 'Test-DefinitionFile Tests' {
 
   }
 
+  It 'GoldenImagePathHasToBeValidUNCPath_ShouldThrow' {
+
+    $rootDir = (get-item $PSScriptRoot).Parent.FullName
+    . "$rootDir\Private\Test-DefinitionFile"
+    . "$rootDir\Private\Classes"
+
+
+    $defFile = "{
+        `"VMs`": [
+            {
+                `"Name`": `"Test`",
+                `"VMHardDiskPath`" : `"C:\\Test`",
+                `"GoldenImagePath`": `"filesystem:\\server\\c$\\Test.vhdx`"
+            }
+        ]
+    }"
+
+    { Test-DefinitionFile -DefinitionJson $defFile } | Should -Throw "Test - GoldenImagePath is not a valid UNC path, UNC paths should start with \\"
+
+  }
+
+  It 'VMHardDiskPathMustBeSetWhenSpecifyingNewVMDiskSizeBytes_ShouldThrow' {
+
+    $rootDir = (get-item $PSScriptRoot).Parent.FullName
+    . "$rootDir\Private\Test-DefinitionFile"
+    . "$rootDir\Private\Classes"
+
+
+    $defFile = "{
+        `"VMs`": [
+            {
+                `"Name`": `"Test`",
+                `"NewVMDiskSizeBytes`": `"1GB`"
+            }
+        ]
+    }"
+
+    { Test-DefinitionFile -DefinitionJson $defFile } | Should -Throw "Test - You must specify VMHardDiskPath when setting NewVMDiskSizeBytes or GoldenImagePath"
+
+  }
+
+  It 'VMHardDiskPathMustBeSetWhenSpecifyingGoldenImagePath_ShouldThrow' {
+
+    $rootDir = (get-item $PSScriptRoot).Parent.FullName
+    . "$rootDir\Private\Test-DefinitionFile"
+    . "$rootDir\Private\Classes"
+
+
+    $defFile = "{
+        `"VMs`": [
+            {
+                `"Name`": `"Test`",
+                `"GoldenImagePath`": `"C:\\Test.vhdx`"
+            }
+        ]
+    }"
+
+    { Test-DefinitionFile -DefinitionJson $defFile } | Should -Throw "Test - You must specify VMHardDiskPath when setting NewVMDiskSizeBytes or GoldenImagePath"
+
+  }
+
+  It 'SpecifyingProvisionScriptsRequiresGoldenImagePathToBeSet_ShouldThrow' {
+
+    $rootDir = (get-item $PSScriptRoot).Parent.FullName
+    . "$rootDir\Private\Test-DefinitionFile"
+    . "$rootDir\Private\Classes"
+
+
+    $defFile = "{
+        `"VMs`": [
+            {
+                `"Name`": `"Test`",
+                `"Provisioning`" : {
+                  `"Scripts`": [
+                      `"C:\\Test.ps1`"
+                  ] 
+              }
+            }
+        ]
+    }"
+
+    { Test-DefinitionFile -DefinitionJson $defFile } | Should -Throw "Test - Provision Scripts can only be used if GoldenImagePath set"
+
+  }
+
+  It 'SpecifyingProvisionScriptsRequiresDeploymentOptionsStartAfterCreate_ShouldThrow' {
+
+    $rootDir = (get-item $PSScriptRoot).Parent.FullName
+    . "$rootDir\Private\Test-DefinitionFile"
+    . "$rootDir\Private\Classes"
+
+    $defFile = "{
+      `"DeploymentOptions`": {
+             `"StartAfterCreation`": false
+             },
+      `"VMs`": [
+          {
+              `"Name`": `"Test`",
+              `"GoldenImagePath`": `"C:\\Test.vhdx`",
+              `"VMHardDiskPath`" : `"C:\\Test`",
+              `"Provisioning`" : {
+                `"Scripts`": [
+                    `"C:\\Test.ps1`"
+                ] 
+            }
+          }
+      ]
+  }"
+
+    { Test-DefinitionFile -DefinitionJson $defFile } | Should -Throw "Test - Provision Scripts can only be used if DeploymentOptions.StartAfterCreation is true"
+
+  }
+
+  It 'ProvisionScriptsMustHavePS1Extension_ShouldThrow' {
+
+    $rootDir = (get-item $PSScriptRoot).Parent.FullName
+    . "$rootDir\Private\Test-DefinitionFile"
+    . "$rootDir\Private\Classes"
+
+    $defFile = "{
+      `"DeploymentOptions`": {
+             `"StartAfterCreation`": false
+             },
+      `"VMs`": [
+          {
+              `"Name`": `"Test`",
+              `"GoldenImagePath`": `"C:\\Test.vhdx`",
+              `"VMHardDiskPath`" : `"C:\\Test`",
+              `"Provisioning`" : {
+                `"Scripts`": [
+                    `"C:\\Test.exe`"
+                ] 
+            }
+          }
+      ]
+  }"
+
+    { Test-DefinitionFile -DefinitionJson $defFile } | Should -Throw "Test - Provision Scripts can only be used if DeploymentOptions.StartAfterCreation is true"
+
+  }
+
 
 }
